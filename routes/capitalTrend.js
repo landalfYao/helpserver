9f60d63aede9028d3fadc4bf5ce9bed7a234d1fa
landalfYao <landalf@sunwou.com>
2019-01-18 18:24:12 +0800

    1

--- routes/capitalTrend.js
+++ routes/capitalTrend.js
@@ -1,17 +1,11 @@
 const router = require('koa-router')()
 const bll = require('./../pub/bll/capitalTrend.js')
 
-router.prefix('/api/capitalTrend')
+router.prefix('/api/ct')
 
-// router.post('/add', async (ctx, next) => {
-//     let result = await bll.add(ctx)
-//     ctx.body = result;
-// })
-// router.post('/get', async (ctx, next) => {
-//     let result = await bll.getList(ctx)
-//     ctx.body = result;
-// })
-
-
+router.post('/get', async (ctx, next) => {
+    let result = await bll.getList(ctx)
+    ctx.body = result;
+})
 
 module.exports = router
