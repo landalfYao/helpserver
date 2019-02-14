29accedd7a6c1c514fee49ab11f1e27831fdb014
landalfYao <landalf@sunwou.com>
2019-02-14 16:07:15 +0800

    Update wxcash.js

--- pub/bll/wxcash.js
+++ pub/bll/wxcash.js
@@ -1,10 +1,12 @@
 const model = require("./../model/cashRecode");
-const com = require("../utils/common");
+const request = require('request');
+// const com = require("../utils/common");
 const config = require('../config/config');
 const cryptoMO = require("crypto"); // MD5算法
 const parseString = require("xml2js").parseString; // xml转js对象
 // const refund = require('./wxrefund')
 const key = config.Mch_key;
+const fs = require('fs');
 const wxpay = {
     async wxcash(ctx, openid, realName, amount) {
         let nonce_str = await this.getNonceStr(); // 随机字符串
@@ -17,14 +19,30 @@ const wxpay = {
         let bodyData = await this.bodyData(openid, nonce_str, partner_trade_no, spbill_create_ip, amount * 100, check_name, re_user_name, desc)
         // 接口
         let urlStr = "https://api.mch.weixin.qq.com/mmpaymkttransfers/promotion/transfers";
-        let cbdata = await com.http.request(urlStr, "POST", bodyData, {})
-        let returnValue = {};
-        if (typeof cbdata == 'string') {
-            let result = await this.parseString(cbdata)
-            console.log(result)
+        // let cbdata = await com.http.request(urlStr, "POST", bodyData, {})
+        let res = new Promise((resolve, reject) => {
+            request({
+                url: urlStr,
+                method: 'POST',
+                body: bodyData,
+                agentOptions: {
+                    pfx: fs.readFileSync(__dirname + '/cert/apiclient_cert.p12'),
+                    passphrase: config.Mch_id
+                }
+            }, function (err, response, body) {
+                if (!err && response.statusCode == 200) {
+                    let data = parser(body);
+                    resolve(data);
+                    console.log(data)
+                } else {
+                    console.log('12', err);
+                }
+            });
 
-        }
-        return returnValue;
+        })
+
+
+        return res;
     },
 
     async parseString(cbdata) {
@@ -74,19 +92,19 @@ const wxpay = {
     async paysignjsapi(appid, mch_id, nonce_str, partner_trade_no, spbill_create_ip,
         amount, check_name, re_user_name, desc, openid) {
         var ret = {
-            appid: appid,
-            mch_id: mch_id,
+            mch_appid: appid,
+            mchid: mch_id,
             nonce_str: nonce_str,
             partner_trade_no: partner_trade_no,
             openid: openid,
-            amount: amount,
-            spbill_create_ip: spbill_create_ip,
             check_name: check_name,
             re_user_name: re_user_name,
-            desc: desc
+            amount: amount,
+            desc: desc,
+            spbill_create_ip: spbill_create_ip
         };
         var str = await this.raw(ret);
-        str = str + "&key=" + key;
+        str = str + "&key=" + config.Mch_key;
         var md5Str = cryptoMO
             .createHash("md5")
             .update(str)
@@ -151,5 +169,25 @@ const wxpay = {
         return (this.S4() + this.S4() + this.S4() + this.S4() + this.S4() + this.S4() + this.S4() + this.S4());
     },
 
+}
+
+function parser(_da) {
+    var xml = _da;
+    xml = xml.slice('<xml>'.length, xml.indexOf('</xml>'));
+    var tag = xml.match(/<\w+>/g);
+    var len = tag.length;
+    var obj = new Object(); //将微信 支付成功 返回的 xml 数据处理为json格式
+    for (var i = 0; i < len; i++) {
+        var node_name = tag[i].replace(/\<|\>/g, '');
+        var tmp = xml.split("<" + node_name + ">");
+        var _tmp = tmp[1].split("</" + node_name + ">");
+        var result = _tmp[0];
+        if (result.match(/^(?!.*CDATA)/)) {
+            obj[node_name] = result;
+        } else {
+            obj[node_name] = result.split('[')[2].split(']')[0];
+        }
+    }
+    return obj;
 }
 module.exports = wxpay
