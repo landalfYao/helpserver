31fe2d8881b68879d364a21d9fcd41931eb2ee6b
landalfYao <landalf@sunwou.com>
2019-02-20 11:56:20 +0800

    1

--- pub/model/dlServer.js
+++ pub/model/dlServer.js
@@ -3,14 +3,14 @@ const db = require('./../db/mysqlHelper.js')
 const roles = {
 
     async add(args) {
-        let sql = 'INSERT INTO dl_server (dl_id,server_name,dl_sy,user_sy,p_sy,is_show,price_gui,des,icon,jdr) value(?,?,?,?,?,?,?,?,?,?)'
-        let params = [args.dl_id, args.server_name, args.dl_sy, args.user_sy, args.p_sy, args.is_show, args.price_gui, args.des, args.icon, args.jdr]
+        let sql = 'INSERT INTO dl_server (dl_id,server_name,dl_sy,user_sy,p_sy,is_show,price_gui,des,icon,jdr,tags) value(?,?,?,?,?,?,?,?,?,?,?)'
+        let params = [args.dl_id, args.server_name, args.dl_sy, args.user_sy, args.p_sy, args.is_show, args.price_gui, args.des, args.icon, args.jdr, args.tags]
         let result = await db.query(sql, params)
         return result
     },
     async update(args) {
-        let sql = 'UPDATE dl_server set dl_sy=?,user_sy=?,p_sy=?,is_show=?,price_gui=?,des=?,icon=?,jdr=? where id = ?'
-        let params = [args.dl_sy, args.user_sy, args.p_sy, args.is_show, args.price_gui, args.des, args.icon, args.jdr, args.id]
+        let sql = 'UPDATE dl_server set dl_sy=?,user_sy=?,p_sy=?,is_show=?,price_gui=?,des=?,icon=?,jdr=?,tags=? where id = ?'
+        let params = [args.dl_sy, args.user_sy, args.p_sy, args.is_show, args.price_gui, args.des, args.icon, args.jdr, args.tags, args.id]
         let result = await db.query(sql, params)
         return result
     },
